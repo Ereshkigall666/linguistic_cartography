6dcc0b1a80d0631a46a1ecb6733648cc4255bff5
Ereshkigall <Ishi.thelema@gmail.com>
2023-04-18 19:25:22 +0200

    fixed vingt map

--- main.R
+++ main.R
@@ -2,4 +2,4 @@ source(file = "ALF_map_functions.R")
 
 create_map(source_file_path = "data/data_Vingt_standard_corr.csv", dest_file_name = "maps/vingt_map.png", title = "VINGT", author = "Marie-Sophie Desmarest", alf_ref = "1398", web = T, subtitle = "final-consonnant-pronunciation")
 
-create_map(source_file_path = "data/data_Moins_standard_corr.csv", dest_file_name = "maps/moins_map.png", title = "MOINS", author = "Marie-Sophie Desmarest", alf_ref = "867", web = T, subtitle = "final-consonnant-pronunciation")
+# create_map(source_file_path = "data/data_Moins_standard_corr.csv", dest_file_name = "maps/moins_map.png", title = "MOINS", author = "Marie-Sophie Desmarest", alf_ref = "867", web = T, subtitle = "final-consonnant-pronunciation")
