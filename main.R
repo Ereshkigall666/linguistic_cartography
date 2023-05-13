e170f50e017cfae72a8383994eb764302e1bae6d
Ereshkigall <Ishi.thelema@gmail.com>
2023-05-13 15:59:32 +0200

    new map for pot

--- main.R
+++ main.R
@@ -1,5 +1,7 @@
 source(file = "ALF_map_functions.R")
 
-create_map(source_file_path = "data/data_Vingt_standard_corr.csv", dest_file_name = "maps/vingt_map.png", title = "VINGT", author = "Marie-Sophie Desmarest", alf_ref = "1398", web = T, subtitle = "final-consonnant-pronunciation")
+# create_map(source_file_path = "data/data_Vingt_standard_corr.csv", dest_file_name = "maps/vingt_map.png", title = "VINGT", author = "Marie-Sophie Desmarest", alf_ref = "1398", web = T, subtitle = "final-consonnant-pronunciation")
 
 # create_map(source_file_path = "data/data_Moins_standard_corr.csv", dest_file_name = "maps/moins_map.png", title = "MOINS", author = "Marie-Sophie Desmarest", alf_ref = "867", web = T, subtitle = "final-consonnant-pronunciation")
+
+create_map(source_file_path = "data/pot_data.csv", dest_file_name = "maps/pot_map.png", title = "POT", author = "Marie-Sophie Desmarest", alf_ref = "1065", web = F, subtitle = "pronunciation of the vowel in POT")
