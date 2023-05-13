3bcac59de80cd1524e6dedac18511426bf4ae989
Ereshkigall <Ishi.thelema@gmail.com>
2023-05-14 00:42:59 +0200

    lemme die

--- main.R
+++ main.R
@@ -4,4 +4,4 @@ source(file = "ALF_map_functions.R")
 
 # create_map(source_file_path = "data/data_Moins_standard_corr.csv", dest_file_name = "maps/moins_map.png", title = "MOINS", author = "Marie-Sophie Desmarest", alf_ref = "867", web = T, subtitle = "final-consonnant-pronunciation")
 
-create_map(source_file_path = "data/pot_data.csv", dest_file_name = "maps/pot_map.png", title = "POT", author = "Marie-Sophie Desmarest", alf_ref = "1065", web = T, subtitle = "pronunciation of the vowel in POT")
+create_map(source_file_path = "data/pot_data.csv", dest_file_name = "maps/pot_map.png", title = "POT", author = "Marie-Sophie Desmarest", alf_ref = "1065", web = T, subtitle = "pronunciation-of-the-vowel-in-POT")
